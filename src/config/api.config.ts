5f5e4f8d6c61b292daa9b774a58c9e92384f9cf0
William Carvalho <williamcoelho.noorden@sulamerica.com.br>
2021-09-25 12:05:04 -0300

    fix: corrigindo bugs da aplicacao

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "https://spring-ionic-nelio.herokuapp.com",
-    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic"
+  baseUrl: "http://localhost:8080",
+  bucketBaseUrl: "https://project-spring-ionic.s3.sa-east-1.amazonaws.com"
 }
